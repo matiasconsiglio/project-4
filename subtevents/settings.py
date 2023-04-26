f07684bde497fd428abad8aca4c58b48840655e3
matiasconsiglio <macastro1@uc.cl>
2023-04-26 17:45:57 +0000

    summernote

--- subtevents/settings.py
+++ subtevents/settings.py
@@ -114,6 +114,7 @@ USE_L10N = True
 USE_TZ = True
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 STATIC_URL = '/static/'
